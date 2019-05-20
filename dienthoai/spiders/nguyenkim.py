a713a54e6b7524abfae68b595b5fff77eb0d7cbe
Minhrobil <nguyenquangminh20111997@gmail.com>
2019-05-20 23:13:34 +0700

    up

--- dienthoai/spiders/nguyenkim.py
+++ dienthoai/spiders/nguyenkim.py
@@ -16,34 +16,13 @@ class QuotesSpider(scrapy.Spider):
             yield Request(url, self.parse)
 
     def parse(self, response):
-        phones = response.css("a.nk-link-product::attr(href)").extract()
-        for phone in phones:
-            yield Request(phone,self.save_info)
-            
+        yield {"url":response.request.url}
+
     # def crawl(self,response):
     #     url = 'https://fptshop.com.vn'
     #     urls_phones = response.css("div.fs-lpil a.fs-lpil-img::attr(href)").getall()
     #     for url_phone in urls_phones:
     #         yield Request(url+url_phone,self.save_info)
             
-    def save_info(self,response):
-        prototypes = response.css('tbody.popup tr').getall()
-        item = {}
-        ten_dien_thoai = response.css("h1::text").get()
-        gia_ban = response.css("span.nk-price-final::text").get()
-        item.update({"ten_dien_thoai":ten_dien_thoai})
-        item.update({"gia_ban":gia_ban})
-        item.update({"link":response.url})
-
-        for prototype in prototypes:
-            prototype = scrapy.Selector(text=prototype)
-            title = prototype.css("td.title::text").get()
-            value = prototype.css("td.value::text").get()
-            item.update({title:value})
-
-        # he_dieu_hanh = scrapy.Selector(text=response.css("div.productInfo_description li").getall()[3]).css("li::text").get()
-        # chipset = scrapy.Selector(text=response.css("div.productInfo_description li").getall()[4]).css("li::text").get()
-        # gpu = scrapy.Selector(text=response.css("div.productInfo_description li").getall()[5]).css("li::text").get()
-        
-        yield item
+   
  
